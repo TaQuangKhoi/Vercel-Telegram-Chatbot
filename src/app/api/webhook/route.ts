3627f5c9baad69578e3f14ab597d91e7a547b31f
Tạ Quang Khôi <45689286+TaQuangKhoi@users.noreply.github.com>
2025-02-22 16:06:52 +0000

    Try add  bot.hears

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,19 +1,30 @@
 import { NextRequest } from 'next/server';
 import { Telegraf } from 'telegraf';
-// import axios from 'axios';
+import axios from 'axios';
 
-// const HTTPS_ENDPOINT = 'https://your-api-endpoint.com/data';
+const HTTPS_ENDPOINT = 'https://app.napkin.one/api/createThought';
 
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN as string);
 
 bot.on('text', async (ctx) => {
   const userMessage = ctx.message.text;
   const data = { user_id: ctx.message.from.id, message: userMessage };
+  console.log(data);
 
   try {
-    // await axios.post(HTTPS_ENDPOINT, data);
-    console.log(data);
-    await ctx.reply('Data sent successfully!');
+    const send_thought_data = await axios.post(HTTPS_ENDPOINT, {
+        email: 'leonardo@davinci.it',
+        token: 'macchina-dettatura-ec1air8wcr',
+        thought: 'The natural desire of good men is knowledge.',
+        sourceUrl: 'https://thevisualagency.com/works/decoding-leonardo-da-vinci-for-the-world/'
+      }, {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+    });
+    
+    await ctx.reply(send_thought_data);
   } catch (error) {
     if (error instanceof Error) {
       await ctx.reply(`Failed to send data: ${error.message}`);
@@ -23,6 +34,7 @@ bot.on('text', async (ctx) => {
   }
   
 });
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
