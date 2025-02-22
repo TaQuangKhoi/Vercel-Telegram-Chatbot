b51f4be236498769e6faecb41792e980a18e5791
Tạ Quang Khôi <45689286+TaQuangKhoi@users.noreply.github.com>
2025-02-22 15:57:16 +0000

    Fix file setWebHook.ts

--- src/app/api/setWebHook/route.ts
+++ src/app/api/setWebHook/route.ts
@@ -1,13 +1,3 @@
-export default async function handler(req, res) {
-    
-  
-    const response = await fetch(
-      `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/setWebhook?url=${webhookUrl}`
-    );
-  
-    const data = await response.json();
-    res.status(200).json(data);
-  }
 
 export async function GET(req: NextRequest) {
     console.log(req);
