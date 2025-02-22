593bdbce8f827e4fe35056d405fc4866c9842cfa
Tạ Quang Khôi <45689286+TaQuangKhoi@users.noreply.github.com>
2025-02-22 15:35:54 +0000

    Remove redandent code

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -2,7 +2,6 @@ import { NextRequest } from 'next/server';
 import { Telegraf } from 'telegraf';
 // import axios from 'axios';
 
-const BOT_TOKEN = process.env.TELEGRAM_TOKEN;
 // const HTTPS_ENDPOINT = 'https://your-api-endpoint.com/data';
 
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN as string);
