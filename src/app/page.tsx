834a6546821b7332e810ea290a1f87a496c877d7
Tạ Quang Khôi <45689286+TaQuangKhoi@users.noreply.github.com>
2025-02-23 09:29:36 +0000

    Run get /api/count/systemThoughts in Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
+import axios from 'axios';
 
-export default function Home() {
+export default async function Home() {
 
-  
-  // fetch('/api/count/systemThoughts')
-  // .then(response => response.json())
-  // .then(data => console.log(data));
+  const data = await axios.get('/api/count/systemThoughts')
+  console.log(data)
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
